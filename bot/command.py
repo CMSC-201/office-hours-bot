33977f12eb01132fa62ff889fa8e8325d251bfb0
Min Chon <snowminc@gmail.com>
2020-03-13 23:56:04 -0400

    fixed substring where it took prefix instead of whole param list

--- bot/command.py
+++ bot/command.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 def parse_arguments(msg, prefix):
     args = []
     if msg.content.startswith(prefix):
-        params = msg.content[:1].split()
+        params = msg.content[1:].split()
         for param in params:
             args.append(param)
     return args
