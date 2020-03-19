aa6c01fcbb5b7d442a322436651653e7909353b3
Min Chon <snowminc@gmail.com>
2020-03-19 14:53:43 -0400

    added office hours toggle functionality

--- bot/command.py
+++ bot/command.py
@@ -21,8 +21,8 @@ def student_waiting(queue, id):
 async def safe_delete(msg, delay=None):
     try:
         await msg.delete(delay=delay)
-    except RuntimeError as e:
-        print(e, "\nBot may proceed normally.")
+    except:
+        print("ERROR when deleting message, but bot may proceed normally.")
 
 
 async def office_close(msg, args, uuids):
@@ -119,6 +119,13 @@ async def request_create(msg, args, uuids):
         await safe_delete(msg)
         return "Executed in wrong channel."
 
+    if not read_json('../offices.json')["is_open"]:
+        text = msg.author.mention + " Office hours are currently closed!"
+        response = await msg.channel.send(text)
+        await safe_delete(response, delay=5)
+        await safe_delete(msg)
+        return msg.author.nick + " requested during closed hours."
+
     student = msg.author.id
     queue = read_json('../student_queue.json')
     # Student already made a request
@@ -168,6 +175,14 @@ async def request_accept(msg, args, uuids):
     
     student_queue = read_json('../student_queue.json')
     office_queue = read_json('../offices.json')
+
+    if not office_queue["is_open"]:
+        text = msg.author.mention + " Office hours was toggled off! Please issue the `!toggle` command!"
+        response = await msg.channel.send(text)
+        await safe_delete(response, delay=5)
+        await safe_delete(msg)
+        return msg.author.nick + " accepted during closed hours."
+
     if len(student_queue) < 1:
         text = msg.author.mention + " There are currently no students needing help!"
         response = await msg.channel.send(text)
@@ -221,6 +236,52 @@ async def request_accept(msg, args, uuids):
         msg.guild.get_channel(office["room"]).name
 
 
+async def toggle_office(msg, args, uuids):
+    # Wrong Channel
+    if msg.channel.id != uuids["RequestsRoom"]:
+        await safe_delete(msg)
+        return "Executed in wrong channel."
+
+    office_queue = read_json('../offices.json')
+    return_msg = ""
+    # Delete the old indicator message if existing
+    if(office_queue["open_indicator"] > 0):
+        indicator_channel = msg.guild.get_channel(uuids["WaitingRoom"])
+        old_msg = await indicator_channel.fetch_message(office_queue["open_indicator"])
+        await safe_delete(old_msg)
+    # Toggle the office hours, send the messages
+    if(office_queue["is_open"]):
+        message = "Office hours are now ""CLOSED**!"
+        waiting = await msg.guild.get_channel(uuids["WaitingRoom"]).send(message)
+        request = await msg.guild.get_channel(uuids["RequestsRoom"]).send(message)
+        office_queue["open_indicator"] = waiting.id
+        office_queue["is_open"] = False
+        return_msg = "Successfully closed office hours."
+    else:
+        message = "Office hours are now **OPEN**!"
+        waiting = await msg.guild.get_channel(uuids["WaitingRoom"]).send(message)
+        request = await msg.guild.get_channel(uuids["RequestsRoom"]).send(message)
+        office_queue["open_indicator"] = waiting.id
+        office_queue["is_open"] = True
+        return_msg = "Successfully opened office hours."
+
+    # Remove all queues. This works in both cases:
+    #   Opening: Clear all old requests before opening
+    #   Closing: Clear all current requests because of closing
+    student_queue = read_json('../student_queue.json')
+    while len(student_queue) > 0:
+        s_info = student_queue.pop()
+        # Delete the request
+        requestmsg = await msg.channel.fetch_message(s_info["requestID"])
+        await safe_delete(requestmsg)
+        
+    # Save state
+    write_json('../student_queue.json', student_queue)
+    write_json('../offices.json', office_queue)
+    # Return log message
+    return return_msg
+
+
 def read_json(path):
     with open(path, "r") as f:
         return json.load(f)
@@ -234,12 +295,17 @@ def write_json(path, data):
 commands = {"close":office_close,
             "auth":student_authenticate,
             "request":request_create,
-            "accept":request_accept}
+            "accept":request_accept,
+            "toggle":toggle_office}
 
 
 async def execute_command(msg, args, uuids):
     cmd = args[0]
     if cmd in commands:
         return await commands[cmd](msg, args, uuids)
+    command_only = [uuids["RequestsRoom"],uuids["WaitingRoom"]]
+    if msg.channel.id in command_only and not msg.author.bot:
+        await safe_delete(msg)
+        return "Not a command, in a command only channel."
     await safe_delete(msg)
     return "Command did not exist."
