85b47c014b75537fae916fb8043fa1f8036e0cb4
Inarchus <eric8@umbc.edu>
2020-08-22 18:12:21 -0400

    Allows new search feature.

--- bot/command/search_user.py
+++ bot/command/search_user.py
@@ -2,28 +2,37 @@ import logging
 
 from discord import Message, Client
 
-import regex
+import re
 import command
+import mongo
 from globals import get_globals
 from queues import QueueAuthority
 from roles import RoleAuthority
+from member import MemberAuthority
+
 
 logger = logging.getLogger(__name__)
 
 
 @command.command_class
 class SearchUsers(command.Command):
-    async def handle(self):
+    __ADMIN_GROUP = 'admin'
+    __TA_GROUP = 'ta'
+    __STUDENTS_GROUP = 'student'
 
+    async def handle(self):
         ra: RoleAuthority = RoleAuthority(self.message.guild)
         if ra.admin:
-            match = regex.match(r'!search\s+user\s+(?P<user_identifier>\w+)', self.message.content)
-            first_name_list = [first_name_user for first_name_user in self.user_collection.find({'First-Name': match.group('user_identifier')})]
-            print(first_name_list)
-            last_name_list = [first_name_user for first_name_user in self.user_collection.find({'Last-Name': match.group('user_identifier')})]
-            print(last_name_list)
-            umbc_id_list = [user for user in self.user_collection.find({'UMBC-Name-Id': match.group('user_identifier')})]
-            print(umbc_id_list)
+            students_group = mongo.db[self.__STUDENTS_GROUP]
+            ta_group = mongo.db[self.__TA_GROUP]
+            admin_group = mongo.db[self.__ADMIN_GROUP]
+
+            for group in [students_group, ta_group, admin_group]:
+                match = re.match(r'!search\s+user\s+(?P<user_identifier>\w+)', self.message.content)
+                first_name_list = [first_name_user for first_name_user in group.find({'First-Name': match.group('user_identifier')})]
+                last_name_list = [first_name_user for first_name_user in group.find({'Last-Name': match.group('user_identifier')})]
+                umbc_id_list = [user for user in group.find({'UMBC-Name-Id': match.group('user_identifier')})]
+                print(first_name_list + last_name_list + umbc_id_list)
 
         await self.message.delete()
 
