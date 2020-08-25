dd4f278808954698a663edd4d68da6e4ad413c2c
Inarchus <eric8@umbc.edu>
2020-08-25 12:20:21 -0400

    Updated to include maintenance channel.  Fixed more bugs.  Put output to the maintenance channel.

--- bot/command/search_user.py
+++ bot/command/search_user.py
@@ -9,7 +9,7 @@ from globals import get_globals
 from queues import QueueAuthority
 from roles import RoleAuthority
 from member import MemberAuthority
-
+from channels import ChannelAuthority
 
 logger = logging.getLogger(__name__)
 
@@ -22,20 +22,28 @@ class SearchUsers(command.Command):
 
     async def handle(self):
         ra: RoleAuthority = RoleAuthority(self.message.guild)
-        if ra.admin:
+        ca: ChannelAuthority = ChannelAuthority(self.message.guild)
+        if ra.admin and ca.is_maintenance_channel(self.message.channel):
             students_group = mongo.db[self.__STUDENTS_GROUP]
             ta_group = mongo.db[self.__TA_GROUP]
             admin_group = mongo.db[self.__ADMIN_GROUP]
 
+            first_name_list = []
+            last_name_list = []
+            umbc_id_list = []
+
             for group in [students_group, ta_group, admin_group]:
                 match = re.match(r'!search\s+user\s+(?P<user_identifier>\w+)', self.message.content)
-                first_name_list = [first_name_user for first_name_user in group.find({'First-Name': match.group('user_identifier')})]
-                last_name_list = [first_name_user for first_name_user in group.find({'Last-Name': match.group('user_identifier')})]
-                umbc_id_list = [user for user in group.find({'UMBC-Name-Id': match.group('user_identifier')})]
-                print(first_name_list + last_name_list + umbc_id_list)
+                first_name_list.extend([first_name_user for first_name_user in group.find({'First-Name': match.group('user_identifier')})])
+                last_name_list.extend([first_name_user for first_name_user in group.find({'Last-Name': match.group('user_identifier')})])
+                umbc_id_list.extend([user for user in group.find({'UMBC-Name-Id': match.group('user_identifier')})])
 
-        # gives a NotFound Exception??
-        # await self.message.delete()
+            combined_list = first_name_list + last_name_list + umbc_id_list
+            message = '\n'.join([str(record) for record in combined_list])
+            if message:
+                await self.message.channel.send(message)
+            else:
+                await self.message.channel.send('No results were found')
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
