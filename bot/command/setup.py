ed99fad865782fbf35ec4506e0b3afd6a6d92547
Ben <doc.ben.johnson@gmail.com>
2020-08-28 09:40:28 -0400

    role bug

--- bot/command/setup.py
+++ bot/command/setup.py
@@ -139,11 +139,11 @@ class SetupCommand(command.Command):
                                                    hoist=True)
         # await admin.edit(position=4)
         logger.info("Created role Admin")
-        ta_role: Role = await guild.create_role(name="TA", permissions=student_permissions, mentionable=True,
+        ta_role: Role = await guild.create_role(name="TA", permissions=ta_permissions, mentionable=True,
                                                 hoist=True)
         # await ta_role.edit(position=3)
         logger.info("Created role TA")
-        student_role: Role = await guild.create_role(name="Student", permissions=ta_permissions, mentionable=True,
+        student_role: Role = await guild.create_role(name="Student", permissions=student_permissions, mentionable=True,
                                                      hoist=True)
         # await student_role.edit(position=2)  # just above @everyone
         logger.info("Created role Student")
