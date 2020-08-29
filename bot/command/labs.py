23a0c91ed0d6aaf15d68d7eb93227a641807b703
inarchus <36237383+inarchus@users.noreply.github.com>
2020-08-29 14:49:27 -0400

    Started check in functionality.

--- bot/command/labs.py
+++ bot/command/labs.py
@@ -1,7 +1,9 @@
 import logging
 
-from discord import Message, Client, Attachment, Guild, PermissionOverwrite
+from discord import Message, Client, Attachment, Guild, PermissionOverwrite, TextChannel, CategoryChannel
+from datetime import datetime, timedelta
 import csv
+import re
 
 import command
 import mongo
@@ -36,15 +38,6 @@ class ConfigureLabs(command.Command):
         """
         ca: ChannelAuthority = ChannelAuthority(self.guild)
         ra: RoleAuthority = RoleAuthority(self.guild)
-
-        """
-            for now delete all old lab sections
-            create all new sections
-            scan TAs and set permissions for TAs assigned to sections
-            scan students and set permissions for students assigned to sections            
-        """
-
-        ra: RoleAuthority = RoleAuthority(self.message.guild)
         if ca.is_maintenance_channel(self.message.channel) and ra.admin in self.message.author.roles:
             pa: PermissionAuthority = PermissionAuthority()
 
@@ -103,6 +96,7 @@ class ConfigureLabs(command.Command):
 
         return False
 
+
 @command.command_class
 class AddToLabSection(command.Command):
     __SECTION = 'Section'
@@ -146,6 +140,7 @@ class AddToLabSection(command.Command):
             return True
         return False
 
+
 @command.command_class
 class RemoveFromLabSection(command.Command):
     __SECTION = 'Section'
@@ -183,9 +178,87 @@ class RemoveFromLabSection(command.Command):
                 ta = self.message.guild.get_member(found_ta[self.__DISCORD_ID])
                 await ca.lab_sections[self.__SECTION_STRING.format(section_number)].set_permissions(ta, overwrite=pa.forbid_overwrite)
 
-
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
         if message.content.startswith('!lab remove user'):
+            return True
+        return False
+
+
+@command.command_class
+class LabCheckIn(command.Command):
+    __SECTION = 'Section'
+    __SECTION_DATA = 'section-data'
+    __DISCORD_ID = 'discord'
+    __SECTION_STRING = 'Lab {}'
+
+    __TA_GROUP = 'ta'
+    __STUDENTS_GROUP = 'student'
+    __USERNAME = 'UMBC-Name-Id'
+    __WEEKDAY_MAP = {0: 'M', 1: 'Tu', 2: 'W', 3: 'Th', 4: 'F', 5: 'Sa', 6: 'Su'}
+
+    async def handle(self):
+        ra: RoleAuthority = RoleAuthority(self.guild)
+        ca: ChannelAuthority = ChannelAuthority(self.guild)
+        # if the student is in the text chat of their lab session and the time is at the start then do the first authentication
+        # if the student is in the text chat of their lab session and the time is after the second login and the TA has started the second check in process.
+
+        current_lab_channel = None
+        the_category: CategoryChannel = self.message.channel.category
+        for lab_name in ca.lab_sections:
+            if the_category == ca.lab_sections[lab_name]:
+                current_lab_channel = ca.lab_sections[lab_name]
+
+        students_group = mongo.db[self.__STUDENTS_GROUP]
+        ta_group = mongo.db[self.__TA_GROUP]
+        section_collection = mongo.db[self.__SECTION_DATA]
+
+        student_record = students_group.find_one({self.__DISCORD_ID: self.message.author.id})
+
+        if student_record and current_lab_channel:
+            section_name = self.__SECTION_STRING.format(student_record[self.__SECTION])
+
+            # we're in the right section, now we need to check that it's the right time
+            if current_lab_channel.name == section_name:
+                section_data = section_collection.find_one({'Section Name': section_name})
+                """ {'_id': ObjectId('5f480a36a6bca0f612b90999'), 'Section': '61', 'Days': 'M', 'Time': '11:00 am - 11:50 am', 'TA': '', 'Nickname': '', 'Section Name': 'Lab 61', 
+                    'Section Category': 748625915226882068, 'Text Channel': 748625915839512646, 'Voice Channel': 748625916711665714}
+                    2020-08-29 13:46:54.248772
+                """
+                if section_data:
+                    margin = timedelta(seconds=30)
+
+                    split_times = section_data['Time'].split('-')
+                    matched_group = re.match(r'(?P<hour>\d+):(?P<minute>\d+)\s+(?P<am_pm>(am|pm))', split_times[0].strip())
+                    today = datetime.today()
+                    # test day for section 61
+                    # today = datetime(year=2020, month=8, day=31, hour=11, minute=3, second=0)
+
+                    the_hour = int(matched_group.group('hour'))
+                    the_minute = int(matched_group.group('minute'))
+
+                    if matched_group.group('am_pm') == 'pm':
+                        the_hour += 12
+                    # combines the current day with the starting time
+                    start_time = datetime(year=today.year, month=today.month, day=today.day,
+                                          hour=the_hour, minute=the_minute, second=0)
+
+                    if start_time - margin <= today <= start_time + timedelta(minutes=15) + margin and \
+                            self.__WEEKDAY_MAP[today.weekday()] in section_data['Days']:
+                        await self.message.author.send('You are checked in to your office hours!')
+                    else:
+                        await self.message.author.send('This is not the correct check in time.')
+
+                    print(section_data, start_time)
+        elif not student_record:
+            await self.message.author.send("We were unable to find you. Talk to you TAs or Professors. ")
+        elif not current_lab_channel:
+            await self.message.author.send("We couldn't find this section.  Did you send this in your lab channel?")
+
+        await self.message.delete()
+
+    @staticmethod
+    async def is_invoked_by_message(message: Message, client: Client):
+        if message.content.startswith('!check in'):
             return True
         return False
