6bf56192138a27896bd07a50d9b1a2c8fee8a611
Christopher Skane <sokrbball@gmail.com>
2020-09-03 15:04:25 -0400

    Created an on-duty TA role to give to TAs, that displays directly above TA.

--- bot/queues.py
+++ bot/queues.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 from discord import Guild, Member, Message, CategoryChannel, NotFound
+import discord.utils
 
-import mongo
+import mongo, logging
 
+logger = logging.getLogger(__name__)
 
 class OHSession:
     def __init__(self, member: Member = None, request: str = None, announcement: Message = None, ta: Member = None,
@@ -196,13 +198,19 @@ class QueueAuthority:
             return False
         return True
 
-    def open_office_hours(self, ta_uid: int) -> bool:
+    """
+	Adds the given ta_uid to the active TA list, and gives the TA the active TA
+	 role. If the role does not exist, it is created, and is placed above TA
+	 role.
+    """
+    async def open_office_hours(self, ta_uid: int) -> bool:
         collection = mongo.db[self.__QUEUE_COLLECTION]
         document = collection.find_one()
         if not document:
             document = {
                 "queue": [],
                 "available_tas": [],
+                "active_role": 0,
                 "open": True,
             }
             collection.insert(document)
@@ -224,10 +232,64 @@ class QueueAuthority:
             fresh_start = True
             is_new_ta = True
 
+        ## Create "Active/on-duty TA" role, if needed
+        if "active_role" not in document:
+            document["active_role"] = 0
+
+        active_role: Role = discord.utils.get(self.guild.roles, id=document["active_role"])
+        if not active_role:
+            active_role: Role = await self.guild.create_role(name="On-duty TA", hoist=True)
+            document["active_role"] = active_role.id
+            logger.info("Created on-duty TA role")
+
+            ## Find TA role and place "On-duty" role above it, but below upper roles
+            move_roles = False
+            ta_pos = 0
+            for i,role in enumerate(self.guild.roles, 0):
+                #logger.info("Role: '%s' @ %d" % (role, role.position))
+                # If we've found the TA role and there is no empty position index, move remaining roles up
+                if ta_pos and (role.position - ta_pos) < 2:
+                    move_roles = True
+                    break
+                elif role.name == "TA" and not ta_pos:
+                    ta_pos = role.position
+                    #logger.info("Found TA role with id %d at index %d!" % (role.id, i))
+
+            ## Update role positions for correct ordering
+            to_move = [active_role] # Active role needs to move regardless
+            to_pos = [ta_pos + 1]
+
+            # Add existing roles to move if necessary
+            if move_roles:
+                existing_roles = self.guild.roles[ta_pos+1:]
+                to_move.extend(existing_roles)
+                to_pos.extend([(r.position+1) for r in existing_roles])
+
+                logger.info("Adding %d existing roles to move.." % (len(existing_roles)))
+
+            move = dict(zip(to_move, to_pos)) # Dictionary of roles and new positions
+
+            # Attempt positions update
+            try:
+                await self.guild.edit_role_positions(positions=move)
+
+                logger.info("Moved on-duty role up")
+            except BaseException as e:
+                logger.error("Error updating roles: %s" % (e))
+
+            #for i,role in enumerate(self.guild.roles, 0):
+            #    logger.info("Role: '%s' @ %d" % (role, role.position))
+
+        # Add TA to role
+        await self.guild.get_member(ta_uid).add_roles(active_role)
+
         collection.replace_one({"_id": document["_id"]}, document)
         return fresh_start, is_new_ta
 
-    def force_close_office_hours(self) -> int:
+    """
+	Closes office hours, and removes the active TA role from all remaining TAs.
+    """
+    async def force_close_office_hours(self) -> int:
         collection = mongo.db[self.__QUEUE_COLLECTION]
         document = collection.find_one()
         if not document:
@@ -238,13 +300,23 @@ class QueueAuthority:
             }
             collection.insert(document)
 
+        # Remove active roles
+        active_role: Role = discord.utils.get(self.guild.roles, id=document["active_role"])
+        if active_role:
+            for ta_uid in document["available_tas"]:
+                await self.guild.get_member(ta_uid).remove_roles(active_role)
+
         document["queue"] = []
         document["available_tas"] = []
         document['open'] = False
 
         collection.replace_one({"_id": document["_id"]}, document)
 
-    def close_office_hours(self, ta_uid: int) -> int:
+    """
+	If present, removes given ta_uid from available TAs and removes the active
+	 TA role from the TA.
+    """
+    async def close_office_hours(self, ta_uid: int) -> int:
         collection = mongo.db[self.__QUEUE_COLLECTION]
         document = collection.find_one()
         if not document:
@@ -261,6 +333,10 @@ class QueueAuthority:
             document["available_tas"].remove(ta_uid)
             was_removed = True
             tas -= 1
+
+            # Remove active role
+            active_role: Role = discord.utils.get(self.guild.roles, id=document["active_role"])
+            await self.guild.get_member(ta_uid).remove_roles(active_role)
 
         collection.replace_one({"_id": document["_id"]}, document)
         return document["open"], was_removed, tas
