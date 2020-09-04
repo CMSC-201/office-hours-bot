b5142aaedba38dea94eed1379bf9365e5e0313cc
inarchus <36237383+inarchus@users.noreply.github.com>
2020-09-04 00:43:56 -0400

    Added bad authentication message for duplicate key usage.

--- bot/command/auth.py
+++ bot/command/auth.py
@@ -18,13 +18,18 @@ class AuthenticateStudent(command.Command):
         member: Member = self.guild.get_member(self.message.author.id)
 
         await self.message.author.send('Starting your authentication process...')
-        if await ma.authenticate_member(member, key):
+        result = await ma.authenticate_member(member, key)
+        if result == MemberAuthority.AUTHENTICATED:
             logger.info("Authenticated user {0.display_name} ({0.id}) as {0.nick}".format(member))
             await self.message.author.send('''You are now authenticated!  You can return to the office hour server.\n  
                                 I live here so I won't actually be going anywhere, but you don't have to talk to me anymore.''')
+        elif result == MemberAuthority.DUPLICATE_ACCOUNT:
+            await self.message.author.send("This key has already been used to authenticate, and is no longer valid.")
         else:
             await self.message.author.send("Key unrecognized.  Please try again.  " +
                                                       "If you're still having trouble, please contact course staff.")
+        if self.message.guild:
+            self.message.delete()
 
     @staticmethod
     async def is_invoked_by_direct_message(message: Message, client: Client):
@@ -46,6 +51,7 @@ class AuthenticateStudent(command.Command):
     async def is_invoked_by_message(message: Message, client: Client):
         if message.content.startswith("!auth resend"):
             await message.author.send('Welcome to Discord Office Hours for CMSC 201, Fall 2020\n I am the 201Bot.\n  Send me a message with !auth (your key pasted here), and we\'ll authenticate you on the channel.')
-        elif message.content.startswith("!auth") and message.guild:
+        elif message.content.startswith("!auth"):
             await message.author.send("Don't share your key via any channel, only send it to me, the discord bot in a DM!")
+            await message.delete(delay=5)
         return False
