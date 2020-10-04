03f0df629ffde47e30599fc811e9d32108989771
inarchus <36237383+inarchus@users.noreply.github.com>
2020-10-04 02:55:35 -0400

    Added some safety checking to ensure removal. Added reset feature.

--- bot/command/remove_user.py
+++ bot/command/remove_user.py
@@ -1,5 +1,7 @@
 import logging
 
+from pymongo.results import DeleteResult, UpdateResult
+
 from discord import Message, Client, Member, User
 from discord.errors import NotFound
 
@@ -32,7 +34,7 @@ class RemoveUser(command.Command):
             admin_group = mongo.db[self.__ADMIN_GROUP]
 
             for group in [students_group, ta_group, admin_group]:
-                match = re.match(r'!remove\s+user\s+(?P<user_identifier>\w+)', self.message.content)
+                match = re.match(r'!remove\s+user\s+(?P<user_identifier>\w+)(\s+(?P<reset>--reset))?', self.message.content)
                 uid = match.group('user_identifier')
                 umbc_id_list = [user for user in group.find({'UMBC-Name-Id': uid})]
 
@@ -44,10 +46,20 @@ class RemoveUser(command.Command):
                         await self.message.channel.send('Deauthenticating User %s' % member.nick)
                         await ma.deauthenticate_member(member)
 
-
-                    await self.message.channel.send('Removing User from Database...')
-                    group.delete_one({self.__UID_FIELD: uid})
-                    await self.message.channel.send('User removal complete.')
+                    if match.group('reset'):
+                        await self.message.channel.send('Resetting User Database Entry...')
+                        result: UpdateResult = group.update_one({self.__UID_FIELD: uid}, {'$set': {self.__DISCORD_ID: ''}})
+                        if result.modified_count:
+                            await self.message.channel.send('User deauthenticated and discord id reset.')
+                        else:
+                            await self.message.channel.send('User deauthenticated but database entry not found/modified.')
+                    else:
+                        await self.message.channel.send('Removing User from Database...')
+                        result: DeleteResult = group.delete_one({self.__UID_FIELD: uid})
+                        if result.deleted_count:
+                            await self.message.channel.send('User removal complete.')
+                        else:
+                            await self.message.channel.send('User deauthenticated but not removed from database.')
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
