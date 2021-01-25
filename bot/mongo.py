04c39bd8f8dbad68dc32ec09a2ef883a1583f2d3
inarchus <36237383+inarchus@users.noreply.github.com>
2021-01-25 17:54:06 -0500

    Reverted file.

--- bot/mongo.py
+++ bot/mongo.py
@@ -9,6 +9,17 @@ logger = logging.getLogger('mongo')
 db = None
 
 
+import json
+import logging
+
+import pymongo
+
+from globals import get_globals
+
+logger = logging.getLogger('mongo')
+db = None
+
+
 def using_mongo():
     props = get_globals()["props"]
     return "mongodb-address" in props and props[
@@ -17,13 +28,7 @@ def using_mongo():
 
 if using_mongo():
     address = get_globals()["props"]['mongodb-address']
-
-    port_number = get_globals()['props'].get('mongo-db-port', 0)
-
     address = address.split("?")[0]
-    # change back for deployment on server
-    client = pymongo.MongoClient('localhost', 27017)
-    # client = pymongo.MongoClient(address + '?retryWrites=false&w=majority')
-
+    client = pymongo.MongoClient(address + '?retryWrites=false&w=majority')
     db_name = address.split("/")[-1]
     db = client[db_name]
