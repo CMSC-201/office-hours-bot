88976d32444b03c62b3923d021111117060335f9
inarchus <36237383+inarchus@users.noreply.github.com>
2021-02-01 15:46:47 -0500

    Added NotFound exception handling for students not yet signed up.

--- bot/command/lab_commands/configure_labs.py
+++ bot/command/lab_commands/configure_labs.py
@@ -1,6 +1,7 @@
 import logging
 
 from discord import Message, Client, Attachment, Guild, PermissionOverwrite, TextChannel, CategoryChannel, Member, File
+from discord.errors import NotFound
 import csv
 
 import command
@@ -9,7 +10,6 @@ from channels import ChannelAuthority
 from roles import RoleAuthority, PermissionAuthority
 
 logger = logging.getLogger(__name__)
-
 
 @command.command_class
 class ConfigureLabs(command.Command):
@@ -26,6 +26,7 @@ class ConfigureLabs(command.Command):
     __ADMIN_GROUP = 'admin'
     __TA_GROUP = 'ta'
     __STUDENTS_GROUP = 'student'
+    __USERNAME = 'UMBC-Name-Id'
 
     async def handle(self):
         """
@@ -45,7 +46,6 @@ class ConfigureLabs(command.Command):
                 students_group = mongo.db[self.__STUDENTS_GROUP]
                 ta_group = mongo.db[self.__TA_GROUP]
                 section_collection = mongo.db[self.__SECTION_DATA]
-
                 await section_data.save(self.__LAB_SECTION_FILE_NAME)
                 with open(self.__LAB_SECTION_FILE_NAME) as lab_sections:
                     section_reader = csv.DictReader(lab_sections)
@@ -59,13 +59,17 @@ class ConfigureLabs(command.Command):
                     section_collection.delete_many({})
 
                     for line in section_reader:
+                        logger.info(str(line))
                         lab_section_name = self.__SECTION_STRING.format(line[self.__SECTION])
-                        ca.lab_sections[line[self.__SECTION]] = await the_guild.create_category(lab_section_name, overwrites={
+                        logger.info('Creating Section Category Channel')
+                        ca.lab_sections[lab_section_name] = await the_guild.create_category(lab_section_name, overwrites={
                             ra.ta: PermissionOverwrite(read_messages=False),
                             ra.student: PermissionOverwrite(read_messages=False),
                             ra.un_authenticated: PermissionOverwrite(read_messages=False)
                         })
+                        logger.info('Creating Section Text')
                         text_channel = await ca.lab_sections[lab_section_name].create_text_channel('Section Text')
+                        logger.info('Creating Section Voice')
                         voice_channel = await ca.lab_sections[lab_section_name].create_voice_channel('Section Voice')
                         line.update({
                             'Section Name': lab_section_name,
@@ -73,19 +77,30 @@ class ConfigureLabs(command.Command):
                             'Text Channel': text_channel.id,
                             'Voice Channel': voice_channel.id
                         })
-
+                        logger.info('Inserting lab into section db')
                         section_collection.insert_one(line)
 
+                        if line[self.__TA_GROUP]:
+                            section_ta = ta_group.find_one({self.__USERNAME: line[self.__TA_GROUP].strip().lower()})
+                            the_ta = await self.get_member(section_ta[self.__DISCORD_ID])
+                            await ca.lab_sections[lab_section_name].set_permissions(the_ta, overwrite=pa.ta_overwrite)
+                            section_ta[self.__SECTION] = line[self.__SECTION]
+                            ta_group.update_one({self.__USERNAME: line[self.__TA_GROUP].strip().lower()}, {'$set': {self.__SECTION: section_ta[self.__SECTION]}})
+
+                        logger.info('Finding TA')
                         for ta in ta_group.find({self.__SECTION: line[self.__SECTION]}):
-                            the_ta = await the_guild.fetch_member(ta[self.__DISCORD_ID])
-                            await ca.lab_sections[line[self.__SECTION]].set_permissions(the_ta, overwrite=pa.ta_overwrite)
+                            the_ta = await self.get_member(ta[self.__DISCORD_ID])
+                            await ca.lab_sections[lab_section_name].set_permissions(the_ta, overwrite=pa.ta_overwrite)
 
+                        logger.info('Finding Students')
                         for student in students_group.find({self.__SECTION: line[self.__SECTION]}):
-                            the_student = await the_guild.fetch_member(student[self.__DISCORD_ID])
-                            await ca.lab_sections[line[self.__SECTION]].set_permissions(the_student, overwrite=pa.student_overwrite)
-
+                            try:
+                                the_student = await the_guild.fetch_member(student[self.__DISCORD_ID])
+                                await ca.lab_sections[lab_section_name].set_permissions(the_student, overwrite=pa.student_overwrite)
+                            except NotFound:
+                                pass
             except Exception as e:
-                print(e)
+                logger.warning(str(e), str(type(e)))
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
