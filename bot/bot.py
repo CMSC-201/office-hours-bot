7daf42a5183662ff94550e2bf52145984f355030
inarchus <36237383+inarchus@users.noreply.github.com>
2021-09-02 12:17:57 -0400

    Added

--- bot/bot.py
+++ bot/bot.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     set_up_logs()
     info = get_globals()
 
-    print(info['props'])
+    logger.info(str(info['props']))
 
     use_submit_daemon = True if 'submit_daemon' in info['props'] and info['props']['submit_daemon'] == 'true' else False
     client = MyClient(submit_daemon=use_submit_daemon)
