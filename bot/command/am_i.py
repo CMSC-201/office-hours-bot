b6101fa63bf7626f939719b75bfc16bcde415f34
inarchus <36237383+inarchus@users.noreply.github.com>
2021-09-27 18:34:14 -0400

    Added the rest of the command.

--- bot/command/am_i.py
+++ bot/command/am_i.py
@@ -6,11 +6,21 @@ import command
 class AmI(command.Command):
     async def handle(self, new_message=None):
         ra: RoleAuthority = RoleAuthority(self.guild)
-        if self.message.content.strip().startswith("!am i an admin"):
+        if self.message.content.strip().lower().startswith("!am i an admin"):
             if ra.is_admin(self.message.author):
                 await self.message.channel.send('Yes you are an admin.')
             else:
                 await self.message.channel.send('No you are not an admin.')
+        elif self.message.content.strip().lower().startswith("!am i a ta"):
+            if ra.is_ta(self.message.author):
+                await self.message.channel.send('Yes you are a ta.')
+            else:
+                await self.message.channel.send('No you are not a ta.')
+        elif self.message.content.strip().lower().startswith("!am i a student"):
+            if ra.is_student(self.message.author):
+                await self.message.channel.send('Yes you are a student.')
+            else:
+                await self.message.channel.send('No you are not a student.')
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
