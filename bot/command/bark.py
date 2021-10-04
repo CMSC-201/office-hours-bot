125fe0a167c11cbf584e350e8cbd76b3d3a61a27
inarchus <36237383+inarchus@users.noreply.github.com>
2021-10-04 05:48:38 -0400

    Allowed unauthed people to send the bark command, in order to verify the functionality of the command.

--- bot/command/bark.py
+++ bot/command/bark.py
@@ -3,8 +3,12 @@ from discord import Message, Client
 import command
 import channels
 
+
 @command.command_class
 class Bark(command.Command):
+    permissions = {'all': True, 'student': True, 'ta': True, 'admin': True}
+
+    @command.Command.authenticate
     async def handle(self):
         await self.message.channel.send("Ruff!")
 
@@ -13,12 +17,14 @@ class Bark(command.Command):
         return message.content.startswith("!bark")
 
     @classmethod
-    def get_help(self):
+    def get_help(cls):
         return 'Bark: This command will elicit a test response from the bot. \n\t!bark'
 
 
 @command.command_class
 class HelloThere(command.Command):
+    permissions = {'student': True, 'ta': True, 'admin': True}
+
     async def handle(self):
         await self.message.channel.send('https://media1.tenor.com/images/2eada1bbeb4ed4182079cf00070324a2/tenor.gif?itemid=13903117')
 
@@ -31,12 +37,14 @@ class HelloThere(command.Command):
         return message.content.startswith("!hello there")
 
     @classmethod
-    def get_help(self):
+    def get_help(cls):
         return 'HelloThere: This command will display Obi Wan Kenobi.\n\t!hello there'
 
 
 @command.command_class
 class Squawk(command.Command):
+    permissions = {'student': True, 'ta': True, 'admin': True}
+
     async def handle(self, new_message=None):
         if new_message:
             if new_message.content.strip().lower() in ['y', 'yes', '!yes']:
@@ -54,7 +62,7 @@ class Squawk(command.Command):
         return message.content.startswith("!squawk")
 
     @classmethod
-    def get_help(self):
+    def get_help(cls):
         return 'This command will elicit a repetition test response from the bot. \n\t!squawk'
 
 
