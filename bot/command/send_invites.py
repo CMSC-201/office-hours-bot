fd30b51a2b496cf1c8d2c047b45333a684811509
inarchus <36237383+inarchus@users.noreply.github.com>
2021-10-23 02:07:19 -0400

    Added new authentication.

--- bot/command/send_invites.py
+++ bot/command/send_invites.py
@@ -33,6 +33,7 @@ class SendInvites(command.Command):
     __EMAIL_SENT = 'email-sent'
     __MONGO_ID = '_id'
 
+    permissions = {'student': False, 'ta': False, 'admin': True}
     state_variables = {__EMAIL_USERNAME: '', __EMAIL_PASSWORD: ''}
 
     def __init__(self, message: Message = None, client: Client = None):
@@ -96,85 +97,81 @@ class SendInvites(command.Command):
             if found_user:
                 group.update_one({self.__MONGO_ID: user[self.__MONGO_ID]}, {'$set': {self.__EMAIL_SENT: code}})
 
+    @command.Command.authenticate
+    @command.Command.require_maintenance
     async def handle(self):
 
-        ra: RoleAuthority = RoleAuthority(self.message.guild)
-        ca: ChannelAuthority = ChannelAuthority(self.message.guild)
+        if self.message.content.startswith('!send invites update'):
 
-        if ra.is_admin(self.message.author) and ca.is_maintenance_channel(self.message.channel):
-            if self.message.content.startswith('!send invites update'):
+            if len(self.message.content.split()) == 5:
+                self.state_variables[self.__EMAIL_USERNAME] = self.message.content.split()[3]
+                self.state_variables[self.__EMAIL_PASSWORD] = self.message.content.split()[4]
 
-                if len(self.message.content.split()) == 5:
-                    self.state_variables[self.__EMAIL_USERNAME] = self.message.content.split()[3]
-                    self.state_variables[self.__EMAIL_PASSWORD] = self.message.content.split()[4]
-
-                    response = self.email_settings_collection.update_one({self.__TYPE: self.__EMAIL_SETTINGS},
-                                                                         {'$set': {self.__EMAIL_USERNAME: self.state_variables[self.__EMAIL_USERNAME],
-                                                                                   self.__EMAIL_PASSWORD: self.state_variables[self.__EMAIL_PASSWORD]}}, upsert=True)
-                    # check how many matches were updated...
-                    await self.message.channel.send('Updated settings for reflector email.')
-                else:
-                    await self.message.channel.send('Unable to update settings for reflector email, use !send invites update <username@gmail.com> <password>')
-                await self.message.delete()
-
-                return
-
-            elif not self.state_variables[self.__EMAIL_USERNAME] and not self.state_variables[self.__EMAIL_PASSWORD]:
-                await self.message.delete()
-                await self.message.channel.send('Unable to send, email and password are not set.  ')
-                return
-
-            group = self.message.content.split()[3].lower()
-            attachment: Attachment = self.message.attachments[0]
-            await attachment.save('office_hour_email.html')
-            with open('office_hour_email.html') as email_file:
-                email_prompt = email_file.read()
-
-            port = 465
-            context = ssl.create_default_context()
-
-            server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
-            try:
-                server.login(self.state_variables[self.__EMAIL_USERNAME], self.state_variables[self.__EMAIL_PASSWORD])
-            except smtplib.SMTPAuthenticationError as smtp_auth_error:
-                await self.message.channel.send(str(smtp_auth_error))
-                return
-
-            users_to_send = []
-            if group in ['all', 'admin', 'tas', 'students']:
-                users_to_send = self.get_user_send_group(group, new=('--new' in self.message.content))
+                response = self.email_settings_collection.update_one({self.__TYPE: self.__EMAIL_SETTINGS},
+                                                                     {'$set': {self.__EMAIL_USERNAME: self.state_variables[self.__EMAIL_USERNAME],
+                                                                               self.__EMAIL_PASSWORD: self.state_variables[self.__EMAIL_PASSWORD]}}, upsert=True)
+                # check how many matches were updated...
+                await self.message.channel.send('Updated settings for reflector email.')
             else:
-                if '-to' in self.message.content:
-                    users_to_send = self.find_users(self.message.content.split('-to')[1])
+                await self.message.channel.send('Unable to update settings for reflector email, use !send invites update <username@gmail.com> <password>')
+            await self.message.delete()
 
-            for user in users_to_send:
-                student_name = ' '.join([user['First-Name'], user['Last-Name']])
-                response = MIMEMultipart('alternative')
-                response['From'] = self.state_variables[self.__EMAIL_USERNAME]
-                response['To'] = '%s@umbc.edu' % user['UMBC-Name-Id']
-                response['Subject'] = '%s, Invitation to Discord Office Hours' % student_name
-                formatted_email_prompt = self.format_email(email_prompt, user)
-                response.attach(MIMEText(formatted_email_prompt, 'html'))
+            return
 
-                try:
-                    await self.message.channel.send('Sending email to %s' % user['UMBC-Name-Id'])
-                    server.sendmail(self.state_variables['email-username'], response['To'], response.as_string())
-                    await asyncio.sleep(4)
-                except smtplib.SMTPRecipientsRefused as smtp_recipients_refused:
-                    print(smtp_recipients_refused)
-                except smtplib.SMTPHeloError as smtp_helo_error:
-                    print(smtp_helo_error)
-                except smtplib.SMTPSenderRefused as smtp_sender_refused:
-                    print(smtp_sender_refused)
-                except smtplib.SMTPDataError as smtp_data_error:
-                    print(smtp_data_error)
-                except smtplib.SMTPNotSupportedError as smtp_not_supported:
-                    print(smtp_not_supported)
+        elif not self.state_variables[self.__EMAIL_USERNAME] and not self.state_variables[self.__EMAIL_PASSWORD]:
+            await self.message.delete()
+            await self.message.channel.send('Unable to send, email and password are not set.  ')
+            return
 
-                await self.message.channel.send('Email invite sent to %s' % student_name)
-                self.update_user_email_status(user, 1)
+        group = self.message.content.split()[3].lower()
+        attachment: Attachment = self.message.attachments[0]
+        await attachment.save('office_hour_email.html')
+        with open('office_hour_email.html') as email_file:
+            email_prompt = email_file.read()
+
+        port = 465
+        context = ssl.create_default_context()
+
+        server = smtplib.SMTP_SSL("smtp.gmail.com", port, context=context)
+        try:
+            server.login(self.state_variables[self.__EMAIL_USERNAME], self.state_variables[self.__EMAIL_PASSWORD])
+        except smtplib.SMTPAuthenticationError as smtp_auth_error:
+            await self.message.channel.send(str(smtp_auth_error))
+            return
+
+        users_to_send = []
+        if group in ['all', 'admin', 'tas', 'students']:
+            users_to_send = self.get_user_send_group(group, new=('--new' in self.message.content))
         else:
-            await self.message.channel.send('You are not an administrator, so cannot run this command.')
+            if '-to' in self.message.content:
+                users_to_send = self.find_users(self.message.content.split('-to')[1])
+
+        for user in users_to_send:
+            student_name = ' '.join([user['First-Name'], user['Last-Name']])
+            response = MIMEMultipart('alternative')
+            response['From'] = self.state_variables[self.__EMAIL_USERNAME]
+            response['To'] = '%s@umbc.edu' % user['UMBC-Name-Id']
+            response['Subject'] = '%s, Invitation to Discord Office Hours' % student_name
+            formatted_email_prompt = self.format_email(email_prompt, user)
+            response.attach(MIMEText(formatted_email_prompt, 'html'))
+
+            try:
+                await self.message.channel.send('Sending email to %s' % user['UMBC-Name-Id'])
+                server.sendmail(self.state_variables['email-username'], response['To'], response.as_string())
+                await asyncio.sleep(4)
+            except smtplib.SMTPRecipientsRefused as smtp_recipients_refused:
+                print(smtp_recipients_refused)
+            except smtplib.SMTPHeloError as smtp_helo_error:
+                print(smtp_helo_error)
+            except smtplib.SMTPSenderRefused as smtp_sender_refused:
+                print(smtp_sender_refused)
+            except smtplib.SMTPDataError as smtp_data_error:
+                print(smtp_data_error)
+            except smtplib.SMTPNotSupportedError as smtp_not_supported:
+                print(smtp_not_supported)
+
+            await self.message.channel.send('Email invite sent to %s' % student_name)
+            self.update_user_email_status(user, 1)
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
