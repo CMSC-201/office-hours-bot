38f9943f7a8701c8792be56edaa870c35eb50223
inarchus <36237383+inarchus@users.noreply.github.com>
2021-12-12 06:10:52 -0500

    Updated remove assignment with the new authentication and channel requirement.  Removed the old admin check which was never implemented.  Removed the unused submit_daemon.updated Flag.

--- bot/command/submit_interface/remove_assignment.py
+++ bot/command/submit_interface/remove_assignment.py
@@ -12,34 +12,27 @@ from roles import RoleAuthority
 
 @command.command_class
 class RemoveAssignment(command.Command):
-    __COMMAND_REGEX = r"!submit\s+configure\s+(?P<assign_name>\w+)\s+remove(\s+(?P<admin>--admin=\w+))?"
+    __COMMAND_REGEX = r"!submit\s+configure\s+(?P<assign_name>\w+)\s+remove"
     __SUBMIT_SYSTEM_ADMINS = 'submit-system-admins'
     __SUBMIT_ASSIGNMENTS = 'submit-assignments'
 
+    permissions = {'student': False, 'ta': False, 'admin': True}
+
+    @command.Command.require_maintenance
+    @command.Command.authenticate
     async def handle(self):
-        ca: ChannelAuthority = ChannelAuthority(self.guild)
-        ra: RoleAuthority = RoleAuthority(self.guild)
-        if ra.is_admin(self.message.author) and ca.is_maintenance_channel(self.message.channel):
-            match = re.match(self.__COMMAND_REGEX, self.message.content)
-            submit_col = mongo.db[self.__SUBMIT_SYSTEM_ADMINS]
-            assignments = mongo.db[self.__SUBMIT_ASSIGNMENTS]
+        match = re.match(self.__COMMAND_REGEX, self.message.content)
+        assignments = mongo.db[self.__SUBMIT_ASSIGNMENTS]
 
-            # keep this for when we need to update on the server.
-            if match.group('admin'):
-                admin_match = submit_col.find_one({'username': match.group('admin')})
-            else:
-                admin_match = submit_col.find_one({})
-
-            assignment_name = match.group('assign_name')
-            dr: DeleteResult = assignments.delete_one({'name': assignment_name})
-            if dr.deleted_count:
-                await self.message.channel.send('Assignment {} has been removed'.format(assignment_name))
-                self.client.submit_daemon.updated = True
-            else:
-                await self.message.channel.send('Assignment {} has not been removed'.format(assignment_name))
+        assignment_name = match.group('assign_name')
+        dr: DeleteResult = assignments.delete_one({'name': assignment_name})
+        if dr.deleted_count:
+            await self.message.channel.send('Assignment {} has been removed'.format(assignment_name))
+        else:
+            await self.message.channel.send('Assignment {} has not been removed'.format(assignment_name))
 
     @staticmethod
     async def is_invoked_by_message(message: Message, client: Client):
-        if re.match(r"!submit\s+configure\s+(?P<assign_name>\w+)\s+remove(\s+(?P<admin>--admin=\w+))?", message.content):
+        if re.match(r"!submit\s+configure\s+(?P<assign_name>\w+)\s+remove", message.content):
             return True
         return False
