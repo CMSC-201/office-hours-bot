8eb968a4b403e17cc58846a2ee628118cfbcecd1
inarchus <36237383+inarchus@users.noreply.github.com>
2024-08-27 08:39:43 -0400

    Changing to logger output.

--- bot/bot.py
+++ bot/bot.py
@@ -34,7 +34,7 @@ class MyClient(discord.Client):
         if len(self.guilds) > 1:
             raise ValueError("Bot cannot manage more than one guild at this time.")
         elif len(self.guilds) == 0:
-            print('The bot is not a member of any guilds.')
+            logger.info('The bot is not a member of any guilds. Exiting...')
             return
 
         self.event_loop = asyncio.get_event_loop()
